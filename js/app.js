0c09b85daca48009d6080f2b5a9fa11373c26380
Michael Busta <busta2356@gmail.com>
2016-07-02 13:41:13 -0400

    append/attr/text img to overlay using jquery

--- js/app.js
+++ js/app.js
@@ -1,12 +1,36 @@
 //Problem: User clicks on image goes to a dead end
 //solution: create an overlay with the large image - Lightbox
 
-//1. Capture the click event on a link to an image
-  //1.1 Show the overlay
-  //1.2 Update overlay with image linked in the link
-  //1.3 Get child's alt attribute and set caption
-//2. Add overlay
-  //2.1 An image
-  //2.2 A caption
-//3. When overlay is clicked
-  //3.1 Hide overlay
+var $overlay = $('<div id="overlay"></div>');
+var $image= $("<img>");
+var $caption = $("<p></p>");
+
+// An image to overlay
+$overlay.append($image);
+
+// A caption to overlay
+$overlay.append($caption);
+
+// Add overlay to body
+$("body").append($overlay);
+
+
+// Capture the click event on a link to an image
+$("#imageGallery a").click(function(event){
+  event.preventDefault();
+  var imageLocation = $(this).attr("href");
+  // Update overlay with image linked in the link
+  $image.attr("src", imageLocation)
+
+// Show the overlay
+  $overlay.show();
+
+// Get child's alt attribute and set caption
+var captionText =  $(this).children("img").attr("alt");
+  $caption.text(captionText);
+});
+// When overlay is clicked
+$overlay.click(function(){
+  //Hide overlay
+  $(this).hide();
+});
